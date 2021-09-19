ab5a8a8d06233ad725e1529ed9d4cac73fadb9e9
Tom Quinlan <tquinlan1992@gmail.com>
2021-09-18 23:34:31 -0400

    test history

--- app/src/components/Header/index.test.tsx
+++ app/src/components/Header/index.test.tsx
@@ -3,24 +3,29 @@ import Header from "./";
 import toJson from "enzyme-to-json";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import SearchContext from "../../searchContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import { toPaths } from "../../pages/routes/paths";
 
 configure({ adapter: new Adapter() });
 
 test("Header", async () => {
   const onSearch = jest.fn();
+  const history = createMemoryHistory();
   const wrapper = mount(
     <SearchContext.Provider value={{ search: "", setSearch: onSearch }}>
-      <Router>
+      <Router history={history}>
         <Header />
       </Router>
     </SearchContext.Provider>
   );
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(toJson(wrapper.find(<Header />))).toMatchSnapshot();
   wrapper
     .find("input")
     .simulate("change", { target: { value: "new search value" } });
   wrapper.find("form").simulate("submit");
   expect(onSearch).toHaveBeenCalledTimes(1);
   expect(onSearch).toHaveBeenCalledWith("new search value");
+  expect(history.length).toBe(2);
+  expect(history.location.pathname).toBe(toPaths.HOME());
 });
